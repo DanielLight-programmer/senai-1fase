001edfdce2e1cb6c492cccf16b6b9bd6296d3751
DanielLight-programmer <danielsfpereira934@gmail.com>
2026-04-16 22:14:12 -0300

    javaScript

---
+++ 01-JavaScript/testes.js
@@ -0,0 +1,38 @@
+// alert("Olá mundo!")
+// alert("SEGUNDA MENSAGEM")
+
+// let nome
+
+// nome = "Rafael"
+
+// alert("nome: " + nome)
+
+// let idade = 30
+// alert("idade: " + idade)
+
+// alert( "Nome: " + nome + "\nIdade: " + idade)
+
+//  nome = prompt("Digite seu nome: ")
+//  alert("Nome digitado : " + nome)
+
+
+
+
+
+
+
+let nota1
+ nota1 = prompt(" Digite a primeira nota: ")
+ nota1 = Number(nota1)
+ nota1 = nota1 + 1
+ alert(nota1)
+
+
+
+
+
+
+
+
+
+
