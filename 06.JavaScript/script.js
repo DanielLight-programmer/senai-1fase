968692016dc7719f0143a16efce80cd24c6f4b67
DanielLight-programmer <danielsfpereira934@gmail.com>
2026-06-16 21:08:33 -0300

    alterção

---
+++ 06.JavaScript/script.js
@@ -0,0 +1,28 @@
+// let contador = 0 
+// while(contador < 10){
+//     alert("Contador: " + contador )
+    
+//     contador++
+// }//voltar e testar novamente
+
+// let peso
+
+// let contador = 0 
+// while(contador < 10)
+// peso = Number(prompt("digite o peso:"))
+// if(peso>1.1 || peso<0.9){
+//     alert("Fora tolerância do peso")
+// }
+// contador++
+
+let total = 0
+
+let i = 0 //inicialização
+while(i < 3){//cond repet
+    i++ //contagem
+    let preco = Number(prompt("Digite o preço do " + i + "° produto: "))
+    total = total + preco
+    document.getElementById('total').innerHTML += i + "° produto R$" + preco.toFixed(2) + "<br>" 
+}
+
+alert(total)
