b7e99f2ed4f7b08c18afdb039cfa0a9155893ca3
DanielLight-programmer <danielsfpereira934@gmail.com>
2026-06-16 22:14:29 -0300

    testes

--- 06-JavaScript/script.js
+++ 06-JavaScript/script.js
@@ -15,14 +15,36 @@
 // }
 // contador++
 
-let total = 0
+// let total = 0
 
-let i = 0 //inicialização
-while(i < 3){//cond repet
-    i++ //contagem
-    let preco = Number(prompt("Digite o preço do " + i + "° produto: "))
-    total = total + preco
-    document.getElementById('total').innerHTML += i + "° produto R$" + preco.toFixed(2) + "<br>" 
+// let i = 0 //inicialização
+// while(i < 3){//cond repet
+//     i++ //contagem
+//     let preco = Number(prompt("Digite o preço do " + i + "° produto: "))
+//     total = total + preco
+//     document.getElementById('total').innerHTML += i + "° produto R$" + preco.toFixed(2) + "<br>" 
+// }
+
+// alert(total)
+// function crescente(){
+//   let numero = 1
+//   while(numero <= 10){
+//     alert(numero)
+//     numero ++
+//   }
+// }
+
+// alert(total)
+// function decrescente(){
+//   let numero = 0
+//   while(numero > 10){
+//     alert(numero)
+//     numero --
+//   }
+// }
+
+let 1+10 //inicialização
+while(1 > 1){//cond repet
+console.log(i):
+i--// contagem
 }
-
-alert(total)
