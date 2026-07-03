df8f8aac00bb5b70ae1dc5b46c78a232b5182597
DANIEL DA SILVA FERNANDES PEREIRA <daniel_sf_pereira@estudante.sesisenai.org.br>
2026-07-02 21:55:42 -0300

    continuando  exercicios de arrys

--- 08-Arrays/script.js
+++ 08-Arrays/script.js
@@ -18,7 +18,7 @@ const nomes = [
 let idades = [22, 33, 32]
 
 let produtos = ["Meia", " Pá de corte", "suco de manga", "leite"]
-let precos = [665, 9999, 777, 12, 4]
+let precos = [665, 9999, 777, 12]
 
 function verProdutos() {
     for (let i = 0; i < produtos.length; i++) {
@@ -43,14 +43,13 @@ function RemoverUltimosProdutos() {
 
 }
 function AumentarTodososPrecosEm300() {
-    // for (let i = 0; i < precos.length; i++) {
-    //     precos[i] = precos[i] * 3
-    //     console.log("aumenta300%" + precos[i])
-
-    //     // document.getElementById('listaProdutos').innerHTML += "<p>" + j + "°: " + produtos[i] + " R$ " + precos[i].toFixed(2).replace('.', ',') + "</p>"
-
-    // }
-  console.log(precos[0] * 4)  
-
+    for (let i = 0; i < precos.length; i++) {
+        precos[i] = precos[i] * 4
+        console.log("aumenta 300% " + precos[i])
+      let  j = i + 1;
+     document.getElementById('listaProdutos').innerHTML += "<p>" + j + " °: " + produtos[i] + " R$ " + precos[i].toFixed(2).replace('.', ',') + "</p>"
+    
+//   console.log(precos[0] * 4)  
+}
 }
 
